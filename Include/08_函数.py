5b285a92ca19e7d978fa876dd68216c13312a995
CorvusCoraxZero <3099725072@qq.com>
2020-10-08 21:46:53 +0800

    10月8日更新学习爬虫proxy相关的知识

--- Include/08_函数.py
+++ Include/08_函数.py
@@ -1,48 +1,58 @@
-'''创建函数'''
+"""创建函数"""
+
+
 def myFirstFunction():
     print("这是我创建的第一个函数")
     print("啦啦啦啦啦啦")
 
+
 def mySecoundFunction(name):
-    print("我要吃"+name)
+    print("我要吃" + name)
 
-def myThirdFunction(name1,name2):
-    print("我要吃"+name1+","+name2)
 
-def add(num1,num2):
-    '这里写的是函数的文档'
-    #我来写点注释
-    return num1+num2
+def myThirdFunction(name1, name2):
+    print("我要吃" + name1 + "," + name2)
 
-def saysome(str1,str2):
-    print('str1是：'+ str1)
+
+def add(num1, num2):
+    """这里写的是函数的文档"""
+    # 我来写点注释
+    return num1 + num2
+
+
+def saysome(str1, str2):
+    print('str1是：' + str1)
     print('str2是：' + str2)
 
-def saysome2(str1="我有默认值",str2="不写也可以"):
-    print('str1是：'+ str1)
+
+def saysome2(str1="我有默认值", str2="不写也可以"):
+    print('str1是：' + str1)
     print('str2是：' + str2)
 
-def test(*params):      #这里参数的类型是收集参数
-    print("参数的长度是：",len(params))
-    print("第二个参数是：",params[1])
 
-def test2(*params,ex="fafa"):      #如果收集参数后面还有参数，赋值的时候要使用关键字参数赋值（建议收集参数后的参数使用默认值参数）
-    print("参数的长度是：",len(params))
-    print("ex参数是：",ex)
+def test(*params):  # 这里参数的类型是收集参数
+    print("参数的长度是：", len(params))
+    print("第二个参数是：", params[1])
+
+
+def test2(*params, ex="fafa"):  # 如果收集参数后面还有参数，赋值的时候要使用关键字参数赋值（建议收集参数后的参数使用默认值参数）
+    print("参数的长度是：", len(params))
+    print("ex参数是：", ex)
+
 
 myFirstFunction()
 mySecoundFunction("鸡腿")
-myThirdFunction("小鱼干","小虾米")
-print(add(8,9))
-print(add.__doc__)  #查看函数的文档
-help(add)           #用help查看函数的文档
+myThirdFunction("小鱼干", "小虾米")
+print(add(8, 9))
+print(add.__doc__)  # 查看函数的文档
+help(add)  # 用help查看函数的文档
 
-saysome(str2="二号字符串",str1="一号字符串")  #用了关键字参数之后，python就不会按顺序来索引了
+saysome(str2="二号字符串", str1="一号字符串")  # 用了关键字参数之后，python就不会按顺序来索引了
 saysome2()
 saysome2(str2="但是也可以改变")
 
-test(1,2,3,4,5,6,7)
-test2(1,2,3,4,5,7,ex="啦啦啦")
+test(1, 2, 3, 4, 5, 6, 7)
+test2(1, 2, 3, 4, 5, 7, ex="啦啦啦")
 
 '''全局变量 局部变量'''
 # def discount(price,rate):
@@ -71,67 +81,88 @@ test2(1,2,3,4,5,7,ex="啦啦啦")
 # print(tt)
 
 '''内部函数'''
+
+
 def fun1():
     print("fun1正在被调用")
-    def fun2():     #fun2是fun1的内部方法，除了fun1内，其他地方都无法调用
+
+    def fun2():  # fun2是fun1的内部方法，除了fun1内，其他地方都无法调用
         print("fun2正在被调用")
+
     fun2()
+
 
 fun1()
 
+
 def funX(x):
     def funY(y):
-        return x * y  #在一个内部函数里，对外部作用域的变量进行了引用，则说该内部函数是一个闭包
+        return x * y  # 在一个内部函数里，对外部作用域的变量进行了引用，则说该内部函数是一个闭包
+
     return funY
 
-#调用闭包
-#方法一：
-i = funX(10) #这里i的类型是一个函数
+
+# 调用闭包
+# 方法一：
+i = funX(10)  # 这里i的类型是一个函数
 print(i(5))
-#方法二：
+# 方法二：
 print(funX(10)(5))
 
 
 def funI(i):
-    i = [i]         #这里使用容器的方法 使得在内部方法中可以对外部变量进行更改 这是Python3之前的办法
+    i = [i]  # 这里使用容器的方法 使得在内部方法中可以对外部变量进行更改 这是Python3之前的办法
+
     def funJ(j):
         # i += j
         # return i  #这里会报错因为i对于funJ来说是一个非全局变量的外部变量 不可以对他进行修改
-        i[0] = i[0]+j
+        i[0] = i[0] + j
         return i[0]
+
     return funJ
+
 
 print(funI(10)(5))
 
+
 def funI(i):
     def funJ(j):
-        nonlocal i #Python3新添加的关键字 用法同global 使得闭包可以对外部变量进行修改
+        nonlocal i  # Python3新添加的关键字 用法同global 使得闭包可以对外部变量进行修改
         i += j
         return i
+
     return funJ
+
 
 print(funI(10)(5))
 
 '''lambda表达式'''
-def ds(x):      #普通的函数
+
+
+def ds(x):  # 普通的函数
     return 2 * x + 1
+
+
 print(ds(5))
-g = lambda x : 2 * x +1 #匿名函数 lambda返回的是一个函数对象
+g = lambda x: 2 * x + 1  # 匿名函数 lambda返回的是一个函数对象
 print(g(5))
-g = lambda x,y: x + y
-print(g(1,4))
+g = lambda x, y: x + y
+print(g(1, 4))
 
 '''
 lambda应用：
 filter() 第一个参数为None的话会筛选出第二个参数中为True的元素 
 如果第一个为函数的话会将第二个参数中的元素放入该函数 并返回结果为True的对象'''
-print(list(filter(None,[1,0,False,True]))) #fliter()方法返回的是一个迭代器对象
+print(list(filter(None, [1, 0, False, True])))  # fliter()方法返回的是一个迭代器对象
+
+
 def odd(x):
     return x % 2
-print(list(filter(odd,(10,3,5,7,8,6,4))))  #这是一个奇数的过滤器
 
-print(list(filter(lambda x: x % 2,(10,3,5,7,8,6,4))))  #lambda明显简介很多
+
+print(list(filter(odd, (10, 3, 5, 7, 8, 6, 4))))  # 这是一个奇数的过滤器
+
+print(list(filter(lambda x: x % 2, (10, 3, 5, 7, 8, 6, 4))))  # lambda明显简介很多
 
 '''map()会将序列中的元素放入函数中加工返回加工完毕的序列'''
-print(list(map(lambda x : x * 2,(1,2,3,4,5))))
-
+print(list(map(lambda x: x * 2, (1, 2, 3, 4, 5))))
--- Include/29_爬虫.py
+++ Include/29_爬虫.py
@@ -1,8 +1,8 @@
-'''
+"""
 url一般由三个部分组成
     第一部分是协议：http, https, ftp, file, ed2k.....
     第二部分是存放资源的服务器的域名系统或者IP地址（有时还需加上端口号）
-    第三部分是资源的具体地址，如目录或者文件名等'''
+    第三部分是资源的具体地址，如目录或者文件名等"""
 """
 Pyrton可以通过urllib这个包 访问互联网
     urllib包中有四个模块，分别是，urllib.request  urllib.error  urllib.parse  urllib.robotparser
@@ -28,51 +28,53 @@ import urllib.request as ulrequest
 # print("保存图片成功")
 
 """实战 利用有道词典来翻译文本"""
-import urllib.request
-import urllib.parse
-import json
-import time
-
-while True:
-
-    content = input("请输入你要翻译的内容(输入'Q!'退出程序)：")
-    if content == 'Q!':
-        break
-
-    url = "http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule"
-
-    #修改heads模拟正常的浏览器访问 方法1
-    '''head = {}
-    head["User-Agent"] = "Mozilla/5.0 (Windows NT 6.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
-    '''
-    data = {}
-    data['type'] = 'AUTO'
-    data['i'] = content
-    data['from'] = 'AUTO'
-    data['to'] = 'AUTO'
-    data['smartresult'] = 'dict'
-    data['client'] = 'fanyideskweb'
-    data['salt'] = '16018840684524'
-    data['sign'] = '7c001b5d1e96aa03e60b5e154c5e3d24'
-    data['lts'] = '1601884068452'
-    data['bv'] = 'bc3c53120c4167be4925b7d40ff2fd56'
-    data['doctype'] = 'json'
-    data['version'] = '2.1'
-    data['keyfrom'] = 'fanyi.web'
-    data['action'] = 'FY_BY_CLICKBUTTION'
-    data = urllib.parse.urlencode(data).encode("utf8")
-
-    head={} #修改heads模拟正常的浏览器访问 方法2
-    req = urllib.request.Request(url, data ,head)
-    #修改heads模拟正常的浏览器访问 方法2
-    req.add_header("User-Agent","Mozilla/5.0 (Windows NT 6.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36")
-
-    response = urllib.request.urlopen(req)
-    html = response.read().decode("utf-8")
-    target = json.loads(html) #将html这个json的数据转换为字典的格式 以便取出其中的内容
-    print(target['translateResult'][0][0]['tgt'])
-
-    time.sleep(3) #延迟时间提交 使得更像是人类访问
+# import urllib.request
+# import urllib.parse
+# import json
+# import time
+#
+# while True:
+#
+#     content = input("请输入你要翻译的内容(输入'Q!'退出程序)：")
+#     if content == 'Q!':
+#         break
+#
+#     url = "http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule"
+#
+#     # 修改heads模拟正常的浏览器访问 方法1
+#     '''head = {}
+#     head["User-Agent"] = "Mozilla/5.0 (Windows NT 6.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " \
+#                          "Chrome/69.0.3497.100 Safari/537.36"
+#     '''
+#     data = {}
+#     data['type'] = 'AUTO'
+#     data['i'] = content
+#     data['from'] = 'AUTO'
+#     data['to'] = 'AUTO'
+#     data['smartresult'] = 'dict'
+#     data['client'] = 'fanyideskweb'
+#     data['salt'] = '16018840684524'
+#     data['sign'] = '7c001b5d1e96aa03e60b5e154c5e3d24'
+#     data['lts'] = '1601884068452'
+#     data['bv'] = 'bc3c53120c4167be4925b7d40ff2fd56'
+#     data['doctype'] = 'json'
+#     data['version'] = '2.1'
+#     data['keyfrom'] = 'fanyi.web'
+#     data['action'] = 'FY_BY_CLICKBUTTION'
+#     data = urllib.parse.urlencode(data).encode("utf8")
+#
+#     head = {}  # 修改heads模拟正常的浏览器访问 方法2
+#     req = urllib.request.Request(url, data, head)
+#     # 修改heads模拟正常的浏览器访问 方法2
+#     req.add_header("User-Agent","Mozilla/5.0 (Windows NT 6.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+#                                 "Chrome/69.0.3497.100 Safari/537.36")
+#
+#     response = urllib.request.urlopen(req)
+#     html = response.read().decode("utf-8")
+#     target = json.loads(html)  # 将html这个json的数据转换为字典的格式 以便取出其中的内容
+#     print(target['translateResult'][0][0]['tgt'])
+#
+#     time.sleep(3)  # 延迟时间提交 使得更像是人类访问
 """
 修改heads模拟正常的浏览器访问有两种方法：
     通过Request的headers参数修改（法一）
@@ -89,3 +91,24 @@ while True:
       4.调用opener
         opener.open(url)
 """
+import urllib.request
+import random
+
+# url = "https://www.whatismyip.com.tw"
+url = "https://ip.tool.chinaz.com/"
+#代理的ip地址
+iplist = ['113.214.13.1:1080','211.137.52.158:8080','211.137.52.159:8080','101.200.211.206:8888']
+
+proxy_support = urllib.request.ProxyHandler({'http':random.choice(iplist)})
+
+# 定制创建一个opener
+opener = urllib.request.build_opener(proxy_support)
+opener.addheaders = [('User-Agent',"Mozilla/5.0 (Windows NT 6.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36")]
+
+# 安装opener
+urllib.request.install_opener(opener)
+
+response = urllib.request.urlopen(url)
+html = response.read().decode('utf-8')
+
+print(html)
